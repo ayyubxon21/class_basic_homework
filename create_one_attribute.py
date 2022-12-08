70dd4902183bcebeaca134023a38fd44bda7856f
ayyubxon <ayyubxonhamdamovtatu@gmail.com>
2022-12-08 04:57:54 -0800

    Evrika

--- create_one_attribute.py
+++ create_one_attribute.py
@@ -1,2 +1,9 @@
 #Create a "Person" class
 #Create an attribute "name" in the "Person" class
+class Person:
+    def __init__(self,name):
+        self.name = name
+    def ism(self):
+        return f"ism{self.name}"
+x = Person(name = 'Ayyubxon')
+print(x.ism())
